91babfc2d72a933b872fe8cd90ac42b5ccf1b427
leadek-nxss <cocoletziyadiel@gmail.com>
2026-08-02 19:36:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,20 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/ping', (req, res) => res.send('ok'));
+app.use(express.static('public'));
 
-let waiting = null;
+let waiting = [];
 
-io.on('connection', socket => {
-  socket.on('findPartner', () => {
-    if (waiting && waiting.id!== socket.id) {
-      const partner = waiting;
-      waiting = null;
-      const room = partner.id + '#' + socket.id;
-      partner.join(room);
-      socket.join(room);
-      partner.partnerRoom = room;
-      socket.partnerRoom = room;
-      io.to(room).emit('matched');
-    } else {
-      waiting = socket;
-      socket.emit('waiting');
-    }
-  });
+function pair(a, b){
+  a.partner = b.id;
+  b.partner = a.id;
+  a.emit('matched');
+  b.emit('matched');
+}
 
-  socket.on('signal', data => {
-    if (socket.partnerRoom) socket.to(socket.partnerRoom).emit('signal', data);
-  });
-
-  socket.on('next', () => {
-    if (socket.partnerRoom) {
-      socket.to(socket.partnerRoom).emit('partnerLeft');
-      socket.leave(socket.partnerRoom);
-    }
-    socket.partnerRoom = null;
-  });
-
-  socket.on('disconnect', () => {
-    if (waiting === socket) waiting = null;
-    if (socket.partnerRoom) socket.to(socket.partnerRoom).emit('partnerLeft');
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('CONAP ON'));
+io.on('connection', (socket) => {
+  console.log
